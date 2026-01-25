381d6f318e8e5c85b95c156556011399cce5af20
Yu-Misaka <109365723+Yu-Misaka@users.noreply.github.com>
2026-01-25 20:42:20 +0800

    refactor: improve clarity of Variety.lean

--- YoungDiagram/Variety.lean
+++ YoungDiagram/Variety.lean
@@ -16,6 +16,15 @@ open Finsupp
 
 namespace Chromosome
 
+/- Comment: tons of Mathlib lemmas rely on partial order for no reason.
+For example `Finsupp.sum_le_sum`, which is obviously still true under pre-order.
+These lemmas could make proving a lot less painful. A pull request in mathlib
+is already opened to address the issue. For the time being we'll just leave a
+sorry here.-/
+lemma filtered_sig_leq (X : Chromosome) (p : Gene → Prop) [DecidablePred p] :
+    signature (X.filter p) ≤ X.signature := by
+  sorry
+
 section lift
 
 noncomputable def lift : Chromosome →+ Chromosome where
@@ -72,6 +81,132 @@ lemma prime_lift_LeftInverse_it (k : ℕ) :
 
 end lift
 
+section IsFiltered
+
+variable {p : Gene → Prop} [DecidablePred p] {X : Chromosome}
+
+variable (p X) in
+def IsFiltered : Prop := X.filter p = X
+
+lemma IsFiltered_def : X.IsFiltered p ↔ X.filter p = X := .rfl
+
+lemma IsFiltered_zero : IsFiltered p 0 := by
+  simp only [IsFiltered, filter_zero]
+
+lemma IsFiltered_single {g : Gene} : IsFiltered p (single g 1) ↔ p g := by
+  simp [IsFiltered]
+  by_cases hg : p g
+  · exact ⟨fun _ ↦ hg, fun _ ↦ filter_single_of_pos _ hg⟩
+  · constructor <;> intro h
+    · symm at h
+      rw [filter_single_of_neg, single_eq_zero] at h <;> tauto
+    · tauto
+
+lemma IsFiltered_add_single {g : Gene} {n : ℕ} (hn : 1 ≤ n) :
+    IsFiltered p (X + single g n) ↔ X.IsFiltered p ∧ p g := by
+  constructor <;> intro h
+  · by_cases hg : p g
+    · simp [IsFiltered, hg] at h
+      exact ⟨h, hg⟩
+    · simp [IsFiltered, hg] at h
+      apply_fun signature at h
+      have := h ▸ (filtered_sig_leq X p)
+      rw [map_add, signature_single g.rank_pos,
+        add_le_iff_nonpos_right, Prod.le_def] at this
+      change n * g.signature.1 ≤ 0 ∧ n * g.signature.2 ≤ 0 at this
+      exact absurd ⟨nonpos_of_mul_nonpos_right this.1 (Rat.natCast_pos.2 hn),
+        nonpos_of_mul_nonpos_right this.2 (Rat.natCast_pos.2 hn)⟩
+        (not_le_of_gt g.signature_pos)
+  · simp [IsFiltered, h, IsFiltered_def.1 h.1]
+
+lemma IsFiltered_iff_add {X Y : Chromosome} :
+    (X + Y).IsFiltered p ↔ X.IsFiltered p ∧ Y.IsFiltered p := by
+  induction Y using Finsupp.induction with
+  | zero =>
+    rw [add_zero]
+    exact (and_iff_left_of_imp fun _ ↦ IsFiltered_zero).symm
+  | single_add g' n f hg' hn hf =>
+    rw [add_comm _ f, ← add_assoc, IsFiltered_add_single
+      (Nat.one_le_iff_ne_zero.2 hn), hf, IsFiltered_add_single
+      (Nat.one_le_iff_ne_zero.2 hn)]
+    tauto
+
+lemma IsFiltered_iff_nsmul {n : ℕ} (hn : n ≠ 0) :
+    (n • X).IsFiltered p ↔ X.IsFiltered p := by
+  induction n using Nat.twoStepInduction with
+  | zero => tauto
+  | one => rw [one_nsmul]
+  | more m _ hm =>
+    specialize hm (by omega)
+    change ((m + 1 + 1) • X).IsFiltered p ↔ _
+    rw [add_nsmul, one_nsmul, IsFiltered_iff_add, hm]
+    tauto
+
+variable (p) in
+def LiftStable : Prop :=
+  ∀ g : Gene, p g ↔ p ⟨g.rank + 1, g.type, Nat.le_add_left 1 g.rank⟩
+
+lemma IsFiltered_iff_lift (hp : LiftStable p) :
+    X.lift.IsFiltered p ↔ X.IsFiltered p := by
+  constructor <;> intro h
+  · induction X using Finsupp.induction
+    · exact IsFiltered_zero
+    · expose_names
+      rw [map_add, IsFiltered_iff_add] at h
+      specialize h_3 h.2
+      refine IsFiltered_iff_add.2 ⟨?_, h_3⟩
+      replace h := h.1
+      simp [lift, liftGene] at h
+      rw [← smul_single_one, IsFiltered_iff_nsmul h_2, IsFiltered_single] at h ⊢
+      exact (hp _).2 h
+  · induction X using Finsupp.induction
+    · exact IsFiltered_zero
+    · expose_names
+      rw [map_add, IsFiltered_iff_add]
+      rw [IsFiltered_iff_add] at h
+      refine ⟨?_, h_3 h.2⟩
+      replace h := h.1
+      simp [lift, liftGene]
+      rw [← smul_single_one, IsFiltered_iff_nsmul h_2, IsFiltered_single] at h ⊢
+      exact (hp _).1 h
+
+variable (p) in
+def varietyOfFilter : variety where
+  carrier := {X : Chromosome | X.IsFiltered p}
+  add_mem' ha hb := IsFiltered_iff_add.2 ⟨ha, hb⟩
+  zero_mem' := IsFiltered_zero
+
+lemma mem_varietyOfFilter_iff {X : Chromosome} :
+  X ∈ varietyOfFilter p ↔ X.IsFiltered p := .rfl
+
+lemma prime_varietyOfFilter (hp : LiftStable p) :
+    (varietyOfFilter p).prime = varietyOfFilter p := by
+  refine le_antisymm ?_ ?_ <;> intro x hx
+  · rw [variety.prime_def, AddSubmonoid.mem_map] at hx
+    rcases hx with ⟨y, ⟨h1, h2⟩⟩
+    rw [mem_varietyOfFilter_iff, ← h2]
+    induction y using Finsupp.induction generalizing x
+    · exact IsFiltered_zero
+    · expose_names
+      rw [mem_varietyOfFilter_iff, IsFiltered_iff_add] at h1
+      rw [map_add, IsFiltered_iff_add]
+      refine ⟨?_, h_2 h1.2 rfl⟩
+      simp [prime, primeGene]
+      split_ifs with h
+      · exact IsFiltered_zero
+      · rw [← smul_single_one, IsFiltered_iff_nsmul h_1, IsFiltered_single] at h1 ⊢
+        rw [hp]
+        convert h1.1
+        refine Nat.sub_add_cancel a.rank_pos
+  · rw [variety.prime_def, AddSubmonoid.mem_map]
+    use x.lift
+    refine ⟨?_, prime_lift_LeftInverse x⟩
+    exact (IsFiltered_iff_lift hp).2 hx
+
+end IsFiltered
+
+section parity
+
 abbrev o (X : Chromosome) : Chromosome := X.filter (Odd  ·.rank)
 abbrev e (X : Chromosome) : Chromosome := X.filter (Even ·.rank)
 
@@ -92,43 +227,26 @@ lemma parityDecomposition (X : Chromosome) : X = X.o + X.e := by
     rw [← Nat.not_odd_iff_even]
   rw [filter_pos_add_filter_neg]
 
-/- Comment: tons of Mathlib lemmas rely on partial order for no reason.
-For example `Finsupp.sum_le_sum`, which is obviously still true under pre-order.
-These lemmas could make proving a lot less painful. A pull request in mathlib
-is already opened to address the issue. For the time being we'll just leave a
-sorry here.-/
-lemma filtered_sig_leq (X : Chromosome) (p : Gene → Prop) [DecidablePred p] :
-    signature (X.filter p) ≤ X.signature := by
-  sorry
+end parity
 
 section polarized
 
-def IsPolarized (X : Chromosome) : Prop :=
-  X.filter (·.type ≠ .NonPolarized) = X
+def IsPolarized (X : Chromosome) : Prop := X.IsFiltered (·.type ≠ .NonPolarized)
 
 lemma IsPolarized_def {X : Chromosome} :
-  X.IsPolarized ↔ X.filter (·.type ≠ .NonPolarized) = X := .rfl
+  X.IsPolarized ↔ X.filter (·.type ≠ .NonPolarized) = X := IsFiltered_def
 
-lemma IsPolarized_zero : IsPolarized 0 := by
-  simp only [IsPolarized_def, ne_eq, filter_zero]
+lemma IsPolarized_zero : IsPolarized 0 := IsFiltered_zero
 
 lemma IsPolarized_single {g : Gene} :
-    IsPolarized (single g 1) ↔ g.type ≠ .NonPolarized := by
-  simp [IsPolarized_def]
-  by_cases hg : g.type = .NonPolarized
-  · constructor <;> intro h
-    · symm at h
-      rw [filter_single_of_neg _ (fun a ↦ a hg), single_eq_zero] at h
-      tauto
-    · tauto
-  · exact ⟨fun _ ↦ hg, fun _ ↦ filter_single_of_pos _ hg⟩
+  IsPolarized (single g 1) ↔ g.type ≠ .NonPolarized := IsFiltered_single
 
 lemma IsPolarized_ofRank {k : ℕ} {ε : GeneType} (hk : 1 ≤ k) :
     (Gene.ofRank k ε).IsPolarized ↔ ε ≠ .NonPolarized := by
   rw [Gene.ofRank_def]
   split_ifs
   · omega
-  · rw [IsPolarized_single]
+  · exact IsPolarized_single
 
 lemma IsPolarized_ofRank' {k : ℕ} {ε : GeneType} (hk : 1 ≤ k) :
     (Gene.ofRank' k ε).IsPolarized ↔ ε ≠ .NonPolarized := by
@@ -138,219 +256,70 @@ lemma IsPolarized_ofRank' {k : ℕ} {ε : GeneType} (hk : 1 ≤ k) :
   · rfl
   · exact GeneType.nonpolarized_iff_neg_non.symm
 
-lemma IsPolarized_add_single {g : Gene} {n : ℕ} (hn : 1 ≤ n) {X : Chromosome} :
-    IsPolarized (X + single g n) ↔ X.IsPolarized ∧ g.type ≠ .NonPolarized := by
-  constructor <;> intro h
-  · by_cases hg : g.type = .NonPolarized
-    · simp [IsPolarized_def, hg] at h
-      apply_fun signature at h
-      have := h ▸ (filtered_sig_leq X (·.type ≠ GeneType.NonPolarized))
-      rw [map_add, signature_single g.rank_pos,
-        add_le_iff_nonpos_right, Prod.le_def] at this
-      change n * g.signature.1 ≤ 0 ∧ n * g.signature.2 ≤ 0 at this
-      exact absurd ⟨nonpos_of_mul_nonpos_right this.1 (Rat.natCast_pos.2 hn),
-        nonpos_of_mul_nonpos_right this.2 (Rat.natCast_pos.2 hn)⟩
-        (not_le_of_gt g.signature_pos)
-    · simp [IsPolarized_def, hg] at h
-      exact ⟨h, hg⟩
-  · simp [IsPolarized_def, h, IsPolarized_def.1 h.1]
-
 lemma IsPolarized_iff_add {X Y : Chromosome} :
-    (X + Y).IsPolarized ↔ X.IsPolarized ∧ Y.IsPolarized := by
-  induction Y using Finsupp.induction with
-  | zero =>
-    rw [add_zero]
-    exact (and_iff_left_of_imp fun _ ↦ IsPolarized_zero).symm
-  | single_add g' n f hg' hn hf =>
-    rw [add_comm _ f, ← add_assoc, IsPolarized_add_single
-      (Nat.one_le_iff_ne_zero.2 hn), hf, IsPolarized_add_single
-      (Nat.one_le_iff_ne_zero.2 hn)]
-    tauto
+  (X + Y).IsPolarized ↔ X.IsPolarized ∧ Y.IsPolarized := IsFiltered_iff_add
 
 lemma IsPolarized_iff_nsmul {X : Chromosome} {n : ℕ} (hn : n ≠ 0) :
-    (n • X).IsPolarized ↔ X.IsPolarized := by
-  induction n using Nat.twoStepInduction with
-  | zero => tauto
-  | one => rw [one_nsmul]
-  | more m _ hm =>
-    specialize hm (by omega)
-    change ((m + 1 + 1) • X).IsPolarized ↔ _
-    rw [add_nsmul, one_nsmul, IsPolarized_iff_add, hm]
-    tauto
+  (n • X).IsPolarized ↔ X.IsPolarized := IsFiltered_iff_nsmul hn
 
 lemma IsPolarized_iff_lift {X : Chromosome} :
-    X.lift.IsPolarized ↔ X.IsPolarized := by
-  constructor <;> intro h
-  · induction X using Finsupp.induction
-    · exact IsPolarized_zero
-    · expose_names
-      rw [map_add, IsPolarized_iff_add] at h
-      specialize h_3 h.2
-      refine IsPolarized_iff_add.2 ⟨?_, h_3⟩
-      replace h := h.1
-      simp [lift, liftGene] at h
-      rwa [← smul_single_one, IsPolarized_iff_nsmul h_2, IsPolarized_single] at h ⊢
-  · induction X using Finsupp.induction
-    · exact IsPolarized_zero
-    · expose_names
-      rw [map_add, IsPolarized_iff_add]
-      rw [IsPolarized_iff_add] at h
-      refine ⟨?_, h_3 h.2⟩
-      replace h := h.1
-      simp [lift, liftGene]
-      rwa [← smul_single_one, IsPolarized_iff_nsmul h_2, IsPolarized_single] at h ⊢
+  X.lift.IsPolarized ↔ X.IsPolarized := IsFiltered_iff_lift (fun _ ↦ .rfl)
 
 end polarized
 
 section nonpolarized
 
-def IsNonPolarized (X : Chromosome) : Prop :=
-  X.filter (·.type = .NonPolarized) = X
+def IsNonPolarized (X : Chromosome) : Prop := X.IsFiltered (·.type = .NonPolarized)
 
 lemma IsNonPolarized_def {X : Chromosome} :
-  X.IsNonPolarized ↔ X.filter (·.type = .NonPolarized) = X := .rfl
+  X.IsNonPolarized ↔ X.filter (·.type = .NonPolarized) = X := IsFiltered_def
 
-lemma IsNonPolarized_zero : IsNonPolarized 0 := by
-  simp only [IsNonPolarized_def, filter_zero]
+lemma IsNonPolarized_zero : IsNonPolarized 0 := IsFiltered_zero
 
 lemma IsNonPolarized_single {g : Gene} :
-    IsNonPolarized (single g 1) ↔ g.type = .NonPolarized := by
-  simp [IsNonPolarized_def]
-  by_cases hg : g.type = .NonPolarized
-  · exact ⟨fun _ ↦ hg, fun _ ↦ filter_single_of_pos _ hg⟩
-  · constructor <;> intro h
-    · symm at h
-      rw [filter_single_of_neg, single_eq_zero] at h <;> tauto
-    · tauto
+  IsNonPolarized (single g 1) ↔ g.type = .NonPolarized := IsFiltered_single
 
 lemma IsNonPolarized_ofRank {k : ℕ} {ε : GeneType} (hk : 1 ≤ k) :
     (Gene.ofRank k ε).IsNonPolarized ↔ ε = .NonPolarized := by
   rw [Gene.ofRank_def]
   split_ifs
   · omega
-  · rw [IsNonPolarized_single]
-
-lemma IsNonPolarized_add_single {g : Gene} {n : ℕ} (hn : 1 ≤ n) {X : Chromosome} :
-    IsNonPolarized (X + single g n) ↔ X.IsNonPolarized ∧ g.type = .NonPolarized := by
-  constructor <;> intro h
-  · by_cases hg : g.type = .NonPolarized
-    · simp [IsNonPolarized_def, hg] at h
-      exact ⟨h, hg⟩
-    · simp [IsNonPolarized_def, hg] at h
-      apply_fun signature at h
-      have := h ▸ (filtered_sig_leq X (·.type = GeneType.NonPolarized))
-      rw [map_add, signature_single g.rank_pos,
-        add_le_iff_nonpos_right, Prod.le_def] at this
-      change n * g.signature.1 ≤ 0 ∧ n * g.signature.2 ≤ 0 at this
-      exact absurd ⟨nonpos_of_mul_nonpos_right this.1 (Rat.natCast_pos.2 hn),
-        nonpos_of_mul_nonpos_right this.2 (Rat.natCast_pos.2 hn)⟩
-        (not_le_of_gt g.signature_pos)
-  · simp [IsNonPolarized_def, h, IsNonPolarized_def.1 h.1]
+  · exact IsNonPolarized_single
 
 lemma IsNonPolarized_iff_add {X Y : Chromosome} :
-    (X + Y).IsNonPolarized ↔ X.IsNonPolarized ∧ Y.IsNonPolarized := by
-  induction Y using Finsupp.induction with
-  | zero =>
-    rw [add_zero]
-    exact (and_iff_left_of_imp fun _ ↦ IsNonPolarized_zero).symm
-  | single_add g' n f hg' hn hf =>
-    rw [add_comm _ f, ← add_assoc, IsNonPolarized_add_single
-      (Nat.one_le_iff_ne_zero.2 hn), hf, IsNonPolarized_add_single
-      (Nat.one_le_iff_ne_zero.2 hn)]
-    tauto
+  (X + Y).IsNonPolarized ↔ X.IsNonPolarized ∧ Y.IsNonPolarized := IsFiltered_iff_add
 
 lemma IsNonPolarized_iff_nsmul {X : Chromosome} {n : ℕ} (hn : n ≠ 0) :
-    (n • X).IsNonPolarized ↔ X.IsNonPolarized := by
-  induction n using Nat.twoStepInduction with
-  | zero => tauto
-  | one => rw [one_nsmul]
-  | more m _ hm =>
-    specialize hm (by omega)
-    change ((m + 1 + 1) • X).IsNonPolarized ↔ _
-    rw [add_nsmul, one_nsmul, IsNonPolarized_iff_add, hm]
-    tauto
+  (n • X).IsNonPolarized ↔ X.IsNonPolarized := IsFiltered_iff_nsmul hn
 
 lemma IsNonPolarized_iff_lift {X : Chromosome} :
-    X.lift.IsNonPolarized ↔ X.IsNonPolarized := by
-  constructor <;> intro h
-  · induction X using Finsupp.induction
-    · exact IsNonPolarized_zero
-    · expose_names
-      rw [map_add, IsNonPolarized_iff_add] at h
-      specialize h_3 h.2
-      refine IsNonPolarized_iff_add.2 ⟨?_, h_3⟩
-      replace h := h.1
-      simp [lift, liftGene] at h
-      rwa [← smul_single_one, IsNonPolarized_iff_nsmul h_2, IsNonPolarized_single] at h ⊢
-  · induction X using Finsupp.induction
-    · exact IsNonPolarized_zero
-    · expose_names
-      rw [map_add, IsNonPolarized_iff_add]
-      rw [IsNonPolarized_iff_add] at h
-      refine ⟨?_, h_3 h.2⟩
-      replace h := h.1
-      simp [lift, liftGene]
-      rwa [← smul_single_one, IsNonPolarized_iff_nsmul h_2, IsNonPolarized_single] at h ⊢
+  X.lift.IsNonPolarized ↔ X.IsNonPolarized := IsFiltered_iff_lift (fun _ ↦ .rfl)
 
 end nonpolarized
 
-def Pi : variety where
-  carrier := {X : Chromosome | IsPolarized X}
-  add_mem' ha hb := IsPolarized_iff_add.2 ⟨ha, hb⟩
-  zero_mem' := IsPolarized_zero
+section Pi
 
-lemma mem_Pi_iff {X : Chromosome} : X ∈ Pi ↔ X.IsPolarized := .rfl
+def Pi : variety := varietyOfFilter (·.type ≠ .NonPolarized)
 
-lemma prime_Pi : Pi.prime = Pi := by
-  refine le_antisymm ?_ ?_ <;> intro x hx
-  · rw [variety.prime_def, AddSubmonoid.mem_map] at hx
-    rcases hx with ⟨y, ⟨h1, h2⟩⟩
-    rw [mem_Pi_iff, ← h2]
-    induction y using Finsupp.induction generalizing x
-    · exact IsPolarized_zero
-    · expose_names
-      rw [mem_Pi_iff, IsPolarized_iff_add, ← @mem_Pi_iff f] at h1
-      rw [map_add, IsPolarized_iff_add]
-      refine ⟨?_, @h_2 (prime f) h1.2 rfl⟩
-      simp [prime, primeGene]
-      split_ifs
-      · exact IsPolarized_zero
-      · rw [← smul_single_one, IsPolarized_iff_nsmul h_1,
-          IsPolarized_single] at h1 ⊢
-        exact h1.1
-  · rw [variety.prime_def, AddSubmonoid.mem_map]
-    use x.lift
-    refine ⟨mem_Pi_iff.2 <| IsPolarized_iff_lift.2 <|
-      mem_Pi_iff.1 hx, prime_lift_LeftInverse x⟩
+lemma mem_Pi_iff {X : Chromosome} :
+  X ∈ Pi ↔ X.IsPolarized := mem_varietyOfFilter_iff
 
-def Lambda : variety where
-  carrier := {X : Chromosome | IsNonPolarized X}
-  add_mem' ha hb := IsNonPolarized_iff_add.2 ⟨ha, hb⟩
-  zero_mem' := IsNonPolarized_zero
+lemma prime_Pi : Pi.prime = Pi := prime_varietyOfFilter (fun _ ↦ .rfl)
 
-lemma mem_Lambda_iff {X : Chromosome} : X ∈ Lambda ↔ X.IsNonPolarized := .rfl
+end Pi
 
-lemma prime_Lambda : Lambda.prime = Lambda := by
-  refine le_antisymm ?_ ?_ <;> intro x hx
-  · rw [variety.prime_def, AddSubmonoid.mem_map] at hx
-    rcases hx with ⟨y, ⟨h1, h2⟩⟩
-    rw [mem_Lambda_iff, ← h2]
-    induction y using Finsupp.induction generalizing x
-    · exact IsNonPolarized_zero
-    · expose_names
-      rw [mem_Lambda_iff, IsNonPolarized_iff_add, ← @mem_Lambda_iff f] at h1
-      rw [map_add, IsNonPolarized_iff_add]
-      refine ⟨?_, @h_2 (prime f) h1.2 rfl⟩
-      simp [prime, primeGene]
-      split_ifs
-      · exact IsNonPolarized_zero
-      · rw [← smul_single_one, IsNonPolarized_iff_nsmul h_1,
-          IsNonPolarized_single] at h1 ⊢
-        exact h1.1
-  · rw [variety.prime_def, AddSubmonoid.mem_map]
-    use x.lift
-    refine ⟨mem_Lambda_iff.2 <| IsNonPolarized_iff_lift.2 <|
-      mem_Lambda_iff.1 hx, prime_lift_LeftInverse x⟩
+section Lambda
+
+def Lambda : variety := varietyOfFilter (·.type = .NonPolarized)
+
+lemma mem_Lambda_iff {X : Chromosome} :
+  X ∈ Lambda ↔ X.IsNonPolarized := mem_varietyOfFilter_iff
+
+lemma prime_Lambda : Lambda.prime = Lambda := prime_varietyOfFilter (fun _ ↦ .rfl)
+
+end Lambda
+
+section Mix
 
 def Mix (v : variety × variety) : variety where
   carrier := {X : Chromosome | X.e ∈ v.1 ∧ X.o ∈ v.2}
@@ -361,6 +330,12 @@ def Mix (v : variety × variety) : variety where
 
 lemma mem_Mix_iff {X : Chromosome} {v : variety × variety} :
   X ∈ Mix v ↔ X.e ∈ v.1 ∧ X.o ∈ v.2 := .rfl
+
+lemma prime_Mix {v : variety × variety} :
+    (Mix v).prime = Mix ⟨v.2.prime, v.1.prime⟩ := by
+  sorry
+
+end Mix
 
 end Chromosome
 
