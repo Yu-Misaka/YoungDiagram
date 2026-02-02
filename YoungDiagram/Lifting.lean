a6a31d789cfce028ff6636c56b53d71afa88c6cd
Yu-Misaka <109365723+Yu-Misaka@users.noreply.github.com>
2026-02-02 16:36:45 +0800

    update section in Lifting.lean

--- YoungDiagram/Lifting.lean
+++ YoungDiagram/Lifting.lean
@@ -1,16 +1,17 @@
-import Mathlib
 import YoungDiagram.Mutations
 
 open Chromosome
 
+noncomputable section
+
 variable (idx : Fin 5) (k : ℕ)
 
-noncomputable abbrev φ := VarietyLabel idx
-noncomputable abbrev φk := VarietyLabel (VarietyLabel.prime^[k] idx)
+abbrev φ := VarietyLabel idx
+abbrev φk := VarietyLabel (VarietyLabel.prime^[k] idx)
 
 variable {X U : Chromosome} (hX : X ∈ φ idx) (hU : U ∈ φk idx k)
 
-noncomputable abbrev LiftingMutationStep :=
+abbrev LiftingMutationStep :=
   MutationStep (VarietyLabel.prime^[k] idx)
     (VarietyLabel.of_mem_prime_iter hX) ⟨U, hU⟩
 
@@ -21,3 +22,5 @@ lemma lifting_property : ∃ Z : Chromosome, (hZ : Z ∈ φ idx) →
     MutationStep idx ⟨X, hX⟩ ⟨Z, hZ⟩ ∧
     prime^[k] Z = U ∧
     ∀ i ≤ k, signature (prime^[k] X) = signature (prime^[k] Z) := sorry
+
+end
