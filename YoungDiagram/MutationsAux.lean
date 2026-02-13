0f44e789ff8a81a92903988c767c8b5ea2d2e8bc
Yu-Misaka <109365723+Yu-Misaka@users.noreply.github.com>
2026-02-13 14:59:40 +0800

    golf

--- YoungDiagram/MutationsAux.lean
+++ YoungDiagram/MutationsAux.lean
@@ -34,71 +34,30 @@ lemma mutation_type1_signature_eq {ε : GeneType} (hε : ε ≠ .NonPolarized)
   have := mutation_type1_iterate_signature_eq hε h_le hm 0 0 (le_of_eq rfl)
   rwa [Function.iterate_zero_apply, Function.iterate_zero_apply, add_zero, add_zero] at this
 
-lemma mutation_type1_le_positive {m n : ℕ} (h_le : m ≤ n) :
-    (Gene.ofRank m .Positive + Gene.ofRank n (- .Positive)) ≤
-    (Gene.ofRank (m - 1) (- .Positive) + Gene.ofRank (n + 1) .Positive) := by
-  rw [le_iff_dominates]
-  intro k
-  simp only [GeneType.neg_pos_eq_neg, iterate_map_add, map_add]
-  repeat rw [prime_iterate_ofRank]
-  by_cases hk1 : k < m
-  · rw [signature_ofRank_positive_eq (by omega), signature_ofRank_positive_eq (by omega)]
-    convert_to _ ≤
-      (Gene.ofRank (m - k - 1) .Negative).signature +
-      ((Gene.ofRank (n - k) .Negative).signature + (1, 0))
-    · congr 3
-      · omega
-      · congr 1; omega
-    abel_nf; rfl
-  by_cases hk2 : k < n
-  · rw [Nat.sub_right_comm, Nat.sub_eq_zero_of_le <| Nat.le_of_not_lt hk1,
-      Nat.zero_sub, signature_ofRank_zero, signature_ofRank_zero, zero_add,
-      zero_add, signature_ofRank_positive_eq (by omega)]
-    convert_to _ ≤ (Gene.ofRank (n - k) .Negative).signature + (1, 0)
-    · congr 3; omega
-    simp; exact posPart_eq_self.mp rfl
-  · rw [Nat.sub_right_comm, Nat.sub_eq_zero_of_le <| Nat.le_of_not_lt hk1,
-      Nat.zero_sub, signature_ofRank_zero, signature_ofRank_zero, zero_add,
-      zero_add, Nat.sub_eq_zero_of_le <| Nat.le_of_not_lt hk2,
-      signature_ofRank_zero]
-    exact signature_nonneg _
-
-lemma mutation_type1_le_negative {m n : ℕ} (h_le : m ≤ n) :
-    (Gene.ofRank m .Negative + Gene.ofRank n (- .Negative)) ≤
-    (Gene.ofRank (m - 1) (- .Negative) + Gene.ofRank (n + 1) .Negative) := by
-  rw [le_iff_dominates]
-  intro k
-  simp only [GeneType.neg_neg_eq_pos, iterate_map_add, map_add]
-  repeat rw [prime_iterate_ofRank]
-  by_cases hk1 : k < m
-  · rw [signature_ofRank_negative_eq (by omega), signature_ofRank_negative_eq (by omega)]
-    convert_to _ ≤
-      (Gene.ofRank (m - k - 1) .Positive).signature +
-      ((Gene.ofRank (n - k) .Positive).signature + (0, 1))
-    · congr 3
-      · omega
-      · congr 1; omega
-    abel_nf; rfl
-  by_cases hk2 : k < n
-  · rw [Nat.sub_right_comm, Nat.sub_eq_zero_of_le <| Nat.le_of_not_lt hk1,
-      Nat.zero_sub, signature_ofRank_zero, signature_ofRank_zero, zero_add,
-      zero_add, signature_ofRank_negative_eq (by omega)]
-    convert_to _ ≤ (Gene.ofRank (n - k) .Positive).signature + (0, 1)
-    · congr 3; omega
-    simp; exact posPart_eq_self.mp rfl
-  · rw [Nat.sub_right_comm, Nat.sub_eq_zero_of_le <| Nat.le_of_not_lt hk1,
-      Nat.zero_sub, signature_ofRank_zero, signature_ofRank_zero, zero_add,
-      zero_add, Nat.sub_eq_zero_of_le <| Nat.le_of_not_lt hk2,
-      signature_ofRank_zero]
-    exact signature_nonneg _
-
 lemma mutation_type1_le {ε : GeneType} (hε : ε ≠ .NonPolarized)
   {m n : ℕ} (h_le : m ≤ n) :
     (Gene.ofRank m ε + Gene.ofRank n (- ε)) ≤
-    (Gene.ofRank (m - 1) (- ε) + Gene.ofRank (n + 1) ε) :=
-  match ε, hε with
-  | .Positive, _ => mutation_type1_le_positive h_le
-  | .Negative, _ => mutation_type1_le_negative h_le
+    (Gene.ofRank (m - 1) (- ε) + Gene.ofRank (n + 1) ε) := by
+  rw [le_iff_dominates]
+  intro k
+  simp only [iterate_map_add, map_add, prime_iterate_ofRank]
+  by_cases hk1 : k < m
+  · rw [signature_ofRank_eq (by omega) hε, signature_ofRank_eq (k := n + 1 - k) (by omega) hε,
+      add_assoc, add_comm (signature (Gene.ofRank 1 ε)), ← add_assoc, ← add_assoc]
+    refine le_of_eq ?_
+    congr 4 <;> omega
+  by_cases hk2 : k < n
+  · rw [Nat.sub_right_comm, Nat.sub_eq_zero_of_le <| Nat.le_of_not_lt hk1,
+      Nat.zero_sub, signature_ofRank_zero, signature_ofRank_zero, zero_add,
+      zero_add, signature_ofRank_eq (k := n + 1 - k) (by omega) hε]
+    convert_to _ ≤ (Gene.ofRank (n - k) (- ε)).signature + signature (Gene.ofRank 1 ε)
+    · congr 3; omega
+    refine le_add_of_le_of_nonneg le_rfl (signature_nonneg _)
+  · rw [Nat.sub_right_comm, Nat.sub_eq_zero_of_le <| Nat.le_of_not_lt hk1,
+      Nat.zero_sub, signature_ofRank_zero, signature_ofRank_zero, zero_add,
+      zero_add, Nat.sub_eq_zero_of_le <| Nat.le_of_not_lt hk2,
+      signature_ofRank_zero]
+    exact signature_nonneg _
 
 end type1_isMutation
 
